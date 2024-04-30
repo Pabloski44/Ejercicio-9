2d179edf7f1fed7e9d9984805f2fdfd45341d1c0
Pabloski44 <101739898+Pabloski44@users.noreply.github.com>
2024-04-30 16:50:24 +0100

    resta añadida

---
+++ Funciones.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Funciones
+    {
+        public void resta(int a, int b)
+        {
+            int calculo;
+            calculo =  a - b;
+            Console.WriteLine("La suma de {0} y {1} es : {2}", a, b, calculo);
+        }
+    }
+}
