7e25b3d33f2741d6745a3ed2fe29130d179db2c6
Pabloski44 <101739898+Pabloski44@users.noreply.github.com>
2024-04-30 17:03:21 +0100

    arreglo resta

--- Funciones.cs
+++ Funciones.cs
@@ -9,11 +9,11 @@ namespace Calculadora
 {
     internal class Funciones
     {
-        public void resta(int a, int b)
+        public int resta(int a, int b)
         {
             int calculo;
             calculo =  a - b;
-            Console.WriteLine("La suma de {0} y {1} es : {2}", a, b, calculo);
+            return calculo;
         }
     }
 }
