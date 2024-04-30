f73c42bfcc65e90ae9316daba80f315d4ea3265e
Yared <150355248+Yaredtl@users.noreply.github.com>
2024-04-30 17:21:22 +0100

    ULTIMO ARREGLO

--- Funciones.cs
+++ Funciones.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Calculadora
+﻿namespace Calculadora
 {
     public class Funciones
     {
-        public int Sumar(int a, int b) 
+        public static int Sumar(int a, int b) 
         {
             int total;
             total = a + b;
             return total;
         }
-        public int Multiplicar(int a, int b)
+        public static int Multiplicar(int a, int b)
         {
             int total;
             total = a * b;
