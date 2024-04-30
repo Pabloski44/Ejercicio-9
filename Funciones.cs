6ec2d01a8604d280e99f3b43686547ed226c9934
Yared <150355248+Yaredtl@users.noreply.github.com>
2024-04-30 16:40:38 +0100

    Suma añadida

---
+++ Funciones.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class Funciones
+    {
+        public Sumar(int a, int b) 
+        {
+            int total;
+            total = a + b;
+            Console.WriteLane("La suma da como resultado " + total);
+        } 
+    }
+}
